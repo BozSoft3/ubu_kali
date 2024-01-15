05f5be7a789221b467f59495a4bddc5e24e9d166
BozSoft3 <156682121+BozSoft3@users.noreply.github.com>
2024-01-15 17:06:21 +0300

    Add files via upload

---
+++ main_inst_scr.sh
@@ -0,0 +1,893 @@
+#!/bin/bash
+				
+apt-get install -y acccheck
+
+							
+apt-get install -y ace-voip
+
+
+apt-get install -y amap
+
+apt-get install -y automater
+
+
+apt-get install -y braa
+
+apt-get install -y casefile
+
+apt-get install -y cdpsnarf
+
+apt-get install -y cisco-torch
+
+apt-get install -y cookie-cadger
+
+apt-get install -y copy-router-config
+
+apt-get install -y dmitry
+
+apt-get install -y dnmap
+
+apt-get install -y dnsenum
+
+apt-get install -y dnsmap
+
+apt-get install -y dnsrecon
+
+apt-get install -y dnstracer
+
+apt-get install -y dnswalk
+
+apt-get install -y dotdotpwn
+
+apt-get install -y enum4linux
+
+apt-get install -y enumiax
+
+apt-get install -y exploitdb
+
+apt-get install -y fierce
+
+apt-get install -y firewalk
+
+apt-get install -y fragroute
+
+apt-get install -y fragrouter
+
+apt-get install -y ghost-phisher
+
+apt-get install -y golismero
+
+apt-get install -y goofile
+
+apt-get install -y lbd
+
+apt-get install -y maltego-teeth
+
+apt-get install -y masscan
+
+apt-get install -y metagoofil
+
+apt-get install -y miranda
+
+apt-get install -y nmap
+
+								
+apt-get install -y p0f
+
+apt-get install -y parsero
+
+apt-get install -y recon-ng
+
+apt-get install -y set
+
+apt-get install -y smtp-user-enum
+
+apt-get install -y snmpcheck
+
+apt-get install -y sslcaudit
+
+apt-get install -y sslsplit
+
+apt-get install -y sslstrip
+
+apt-get install -y sslyze
+
+apt-get install -y thc-ipv6
+
+apt-get install -y theharvester
+
+apt-get install -y tlssled
+
+apt-get install -y twofi
+
+apt-get install -y urlcrazy
+
+apt-get install -y wireshark
+
+apt-get install -y wol-e
+
+apt-get install -y xplico
+
+apt-get install -y ismtp
+
+apt-get install -y intrace
+
+apt-get install -y hping3
+							
+apt-get install -y bbqsql
+
+
+apt-get install -y bed
+
+
+apt-get install -y cisco-auditing-tool
+
+apt-get install -y cisco-global-exploiter
+
+apt-get install -y cisco-ocs
+
+apt-get install -y cisco-torch
+
+apt-get install -y copy-router-config
+
+
+
+apt-get install -y doona
+
+apt-get install -y dotdotpwn
+
+apt-get install -y greenbone-security-assistant
+
+
+							
+apt-get install -y hexorbase
+							
+							
+apt-get install -y jsql
+							
+apt-get install -y lynis
+							
+apt-get install -y nmap
+							
+apt-get install -y ohrwurm
+							
+apt-get install -y openvas-administrator
+							
+apt-get install -y openvas-cli
+							
+apt-get install -y openvas-manager
+							
+apt-get install -y openvas-scanner
+							
+apt-get install -y oscanner
+							
+apt-get install -y powerfuzzer
+							
+apt-get install -y sfuzz
+							
+apt-get install -y sidguesser
+							
+apt-get install -y siparmyknife
+							
+apt-get install -y sqlmap
+							
+apt-get install -y sqlninja
+							
+apt-get install -y sqlsus
+							
+apt-get install -y thc-ipv6
+							
+apt-get install -y tnscmd10g
+							
+apt-get install -y unix-privesc-check
+							
+apt-get install -y yersinia
+							
+apt-get install -y aircrack-ng
+
+							
+apt-get install -y asleap
+
+							
+apt-get install -y bluelog
+							
+
+							
+apt-get install -y blueranger
+							
+apt-get install -y bluesnarfer
+							
+apt-get install -y bully
+							
+apt-get install -y cowpatty
+							
+apt-get install -y crackle
+							
+apt-get install -y eapmd5pass
+							
+apt-get install -y fern-wifi-cracker
+							
+apt-get install -y ghost-phisher
+							
+apt-get install -y giskismet
+							
+
+							
+apt-get install -y kalibrate-rtl
+							
+apt-get install -y killerbee
+							
+apt-get install -y kismet
+							
+apt-get install -y mdk3
+							
+apt-get install -y mfcuk
+							
+apt-get install -y mfoc
+							
+apt-get install -y mfterm
+							
+apt-get install -y multimon-ng
+							
+apt-get install -y pixiewps
+							
+apt-get install -y reaver
+							
+apt-get install -y redfang
+							
+apt-get install -y rtlsdr-scanner
+							
+apt-get install -y spooftooph
+							
+apt-get install -y wifi-honey
+							
+apt-get install -y wifitap
+							
+apt-get install -y wifite
+							
+apt-get install -y aircrack-ng asleap bluelog blueranger bluesnarfer bully cowpatty crackle eapmd5pass fern-wifi-cracker ghost-phisher giskismet gqrx kalibrate-rtl killerbee kismet mdk3 mfcuk mfoc mfterm multimon-ng pixiewps reaver redfang spooftooph wifi-honey wifitap wifite
+						
+apt-get install -y apache-users
+
+							
+apt-get install -y arachni
+
+							
+apt-get install -y bbqsql
+							
+apt-get install -y blindelephant
+							
+apt-get install -y burpsuite
+							
+apt-get install -y cutycapt
+							
+
+							
+apt-get install -y davtest
+							
+apt-get install -y deblaze
+							
+apt-get install -y dirb
+							
+apt-get install -y dirbuster
+							
+apt-get install -y fimap
+							
+apt-get install -y funkload
+							
+apt-get install -y grabber
+							
+apt-get install -y jboss-autopwn
+							
+apt-get install -y joomscan
+							
+apt-get install -y jsql
+							
+apt-get install -y maltego-teeth
+							
+apt-get install -y padbuster
+							
+apt-get install -y paros
+							
+apt-get install -y parsero
+							
+apt-get install -y plecost
+							
+apt-get install -y powerfuzzer
+							
+apt-get install -y proxystrike
+							
+apt-get install -y recon-ng
+							
+apt-get install -y skipfish
+							
+apt-get install -y sqlmap
+							
+apt-get install -y sqlninja
+							
+apt-get install -y sqlsus
+							
+apt-get install -y ua-tester
+							
+apt-get install -y uniscan
+							
+apt-get install -y vega
+							
+apt-get install -y w3af
+							
+apt-get install -y webscarab
+							
+							
+
+							
+apt-get install -y websploit
+							
+apt-get install -y wfuzz
+							
+apt-get install -y wpscan
+							
+apt-get install -y xsser
+							
+apt-get install -y zaproxy										
+						
+apt-get install -y burpsuite
+
+							
+apt-get install -y dnschef
+
+							
+apt-get install -y fiked
+							
+apt-get install -y hamster-sidejack
+							
+apt-get install -y hexinject
+							
+apt-get install -y iaxflood
+							
+apt-get install -y inviteflood
+							
+apt-get install -y ismtp
+							
+
+							
+apt-get install -y mitmproxy
+							
+apt-get install -y ohrwurm
+							
+apt-get install -y protos-sip
+							
+apt-get install -y rebind
+							
+apt-get install -y responder
+							
+apt-get install -y rtpbreak
+							
+apt-get install -y rtpinsertsound
+							
+apt-get install -y rtpmixsound
+							
+apt-get install -y sctpscan
+							
+apt-get install -y siparmyknife
+							
+apt-get install -y sipp
+							
+apt-get install -y sipvicious
+							
+apt-get install -y sniffjoke
+							
+apt-get install -y sslsplit
+							
+apt-get install -y sslstrip
+							
+apt-get install -y thc-ipv6
+							
+apt-get install -y voiphopper
+							
+apt-get install -y webscarab
+							
+apt-get install -y wifi-honey
+							
+apt-get install -y wireshark
+							
+apt-get install -y xspy
+							
+apt-get install -y yersinia
+							
+apt-get install -y zaproxy
+							
+apt-get install -y cryptcat
+
+							
+apt-get install -y cymothoa
+
+							
+apt-get install -y dbd
+							
+apt-get install -y dns2tcp":
+							
+apt-get install -y http-tunnel
+							
+apt-get install -y httptunnel
+							
+apt-get install -y intersect
+							
+apt-get install -y nishang
+							
+apt-get install -y polenum
+							
+apt-get install -y powersploit
+							
+apt-get install -y pwnat
+							
+apt-get install -y ridenum
+							
+apt-get install -y sbd
+							
+apt-get install -y u3-pwn
+							
+apt-get install -y webshells
+							
+apt-get install -y weevely
+							
+apt-get install -y casefile
+
+							
+apt-get install -y cutycapt
+
+							
+apt-get install -y dos2unix
+							
+apt-get install -y dradis
+							
+apt-get install -y keepnote
+							
+apt-get install -y magictree
+							
+apt-get install -y metagoofil
+							
+apt-get install -y nipper-ng
+							
+apt-get install -y pipal
+						
+apt-get install -y armitage
+
+							
+apt-get install -y backdoor-factory
+
+							
+apt-get install -y beef-xss
+							
+apt-get install -y cisco-auditing-tool
+							
+apt-get install -y cisco-global-exploiter
+							
+apt-get install -y cisco-ocs
+							
+apt-get install -y cisco-torch
+							
+
+							
+apt-get install -y crackle
+							
+apt-get install -y jboss-autopwn
+							
+apt-get install -y linux-exploit-suggester
+							
+apt-get install -y maltego-teeth
+							
+apt-get install -y set
+							
+apt-get install -y shellnoob
+							
+apt-get install -y sqlmap
+							
+apt-get install -y thc-ipv6
+							
+apt-get install -y yersinia
+						
+apt-get install -y binwalk
+
+							
+apt-get install -y bulk-extractor
+
+							
+
+							
+apt-get install -y chntpw
+							
+apt-get install -y cuckoo
+							
+apt-get install -y dc3dd
+							
+apt-get install -y ddrescue
+							
+							
+
+							
+apt-get install -y dumpzilla
+							
+apt-get install -y extundelete
+							
+apt-get install -y foremost
+							
+apt-get install -y galleta
+							
+apt-get install -y guymager
+							
+apt-get install -y iphone-backup-analyzer
+							
+apt-get install -y p0f
+							
+apt-get install -y pdf-parser
+							
+apt-get install -y pdfid
+							
+apt-get install -y pdgmail
+							
+apt-get install -y peepdf
+							
+					
+							
+apt-get install -y volatility
+							
+apt-get install -y xplico
+							
+apt-get install -y dhcpig
+
+							
+apt-get install -y funkload
+
+							
+apt-get install -y iaxflood
+							
+
+							
+apt-get install -y inviteflood
+							
+apt-get install -y ipv6-toolkit
+							
+apt-get install -y mdk3
+							
+apt-get install -y reaver
+							
+apt-get install -y rtpflood
+							
+apt-get install -y slowhttptest
+							
+apt-get install -y t50
+							
+apt-get install -y termineter
+							
+apt-get install -y thc-ipv6
+							
+apt-get install -y thc-ssl-dos     				             										
+							
+				
+apt-get install -y acccheck
+
+							
+apt-get install -y burpsuite
+
+							
+apt-get install -y cewl
+							
+apt-get install -y chntpw
+							
+apt-get install -y cisco-auditing-tool
+							
+apt-get install -y cmospwd
+							
+apt-get install -y creddump
+							
+apt-get install -y crunch
+							
+
+							
+apt-get install -y findmyhash
+							
+apt-get install -y gpp-decrypt
+							
+apt-get install -y hash-identifier
+							
+apt-get install -y hexorbase
+							
+							
+apt-get install -y john
+							
+apt-get install -y johnny
+							
+apt-get install -y keimpx
+							
+apt-get install -y maltego-teeth
+							
+apt-get install -y maskprocessor
+							
+apt-get install -y multiforcer
+							
+apt-get install -y ncrack
+							
+apt-get install -y oclgausscrack
+							
+apt-get install -y pack
+							
+apt-get install -y patator
+							
+
+							
+apt-get install -y polenum
+							
+apt-get install -y rainbowcrack
+							
+apt-get install -y rcracki-mt
+							
+apt-get install -y rsmangler
+							
+								
+							
+apt-get install -y statsprocessor
+							
+apt-get install -y thc-pptp-bruter
+							
+apt-get install -y truecrack
+							
+apt-get install -y webscarab
+							
+apt-get install -y wordlists
+							
+apt-get install -y zaproxy
+					
+apt-get install -y apktool
+
+							
+apt-get install -y dex2jar
+
+							
+apt-get install -y python-diStorm3
+							
+apt-get install -y edb-debugger
+							
+apt-get install -y jad
+							
+apt-get install -y javasnoop
+							
+apt-get install -y JD
+							
+apt-get install -y OllyDbg
+							
+apt-get install -y smali
+							
+apt-get install -y Valgrind
+							
+apt-get install -y YARA
+						
+apt-get install -y android-sdk
+
+							
+apt-get install -y apktool
+
+							
+apt-get install -y arduino
+							
+apt-get install -y dex2jar
+							
+apt-get install -y sakis3g
+							
+apt-get install -y smali
+
+
+					
+apt-get install -y squid3
+	
+
+apt-get installaircrack-ng -y
+apt-get installwpscan -y
+apt-get installnmap -y
+apt-get installace -y
+apt-get installipv6toolkit -y
+apt-get installexploitdb -y
+apt-get installarp-scan -y
+apt-get installdmitry -y
+apt-get installike-scan -y
+apt-get installlegion -y
+apt-get installmaltego -y
+apt-get installnetdiscover -y
+apt-get installp0f -y
+apt-get installrecon-ng -y
+apt-get installspiderfoot -y
+apt-get installdnsenum -y
+apt-get installdnsmap -y
+apt-get installdnsrecon -y
+apt-get installdnstracer -y
+apt-get installdnswalk -y
+apt-get installfierce -y
+apt-get installmaltego -y
+apt-get installspiderfoot -y
+apt-get installtwofi -y
+apt-get install0trace -y
+apt-get installirpas -y
+apt-get installnetmask -y
+apt-get installnbtscan -y
+apt-get installsmbmap -y
+apt-get installsmtp-user-enum -y
+apt-get installswaks -y
+apt-get installbraa -y
+apt-get installssldump -y
+apt-get installsslh -y
+apt-get installsslscan -y
+apt-get installtlssled -y
+apt-get installarping -y
+apt-get installfping -y
+apt-get installhping3 -y
+apt-get installmasscan -y
+apt-get installunicornscan -y
+apt-get installxprobe2 -y
+apt-get installlbd -y
+apt-get installwafw00f -y
+apt-get installlynis -y
+apt-get installnikto -y
+apt-get installdhcpig -y
+apt-get installiaxflood -y
+apt-get installsiege -y
+apt-get installt50 -y
+apt-get installcisco-auditing-tool -y
+apt-get installcisco-global-exploiter -y
+apt-get installcisco-ocs -y
+apt-get installcisco-torch -y
+apt-get installcopy-router-config -y
+apt-get installyersinia -y
+apt-get installbed -y
+apt-get installsiparmyknife -y
+apt-get installspike -y
+apt-get installenumiax -y
+apt-get installiaxflood -y
+apt-get installrtpflood -y
+apt-get installrtpinsertsound -y
+apt-get installrtpmixsound -y
+apt-get installsctpscan -y
+apt-get installsipp -y
+apt-get installburpsuite -y
+apt-get installcommix -y
+apt-get installhttrack. -y
+apt-get installparos -y
+apt-get installskipfish -y
+apt-get installsqlmap -y
+apt-get installwebscarab -y
+apt-get installapache-users -y
+apt-get installcutycapt -y
+apt-get installdirb -y
+apt-get installdirbuster -y
+apt-get installuniscan -y
+apt-get installwfuzz -y
+apt-get installjboss-autopwn -y
+apt-get installjoomscan -y
+apt-get installplecost -y
+apt-get installdavtest -y
+apt-get installjsql -y
+apt-get installpadbuster -y
+apt-get installskipfish -y
+apt-get installwhatweb -y
+apt-get installxsser -y
+apt-get installmdb-sql -y
+apt-get installoscanner -y
+apt-get installsidguesser -y
+apt-get installsqldic -y
+apt-get installsqlmap -y
+apt-get installsqlninja -y
+apt-get installtnscmd10g -y
+apt-get installcewl -y
+apt-get installcrunch -y
+apt-get installhashcat -y
+apt-get installjohn -y
+apt-get installmedusa -y
+apt-get installophcrack -y
+apt-get installrainbowcrack -y
+apt-get installrcracki-mt -y
+apt-get installwordlists -y
+apt-get installhydra -y
+apt-get installpatator -y
+apt-get installthc-pptp-bruter -y
+apt-get installchntpw -y
+apt-get installcmospwd -y
+apt-get installfcrackzip -y
+apt-get installhashid -y
+apt-get installhash-identifier -y
+apt-get installophcrack -y
+apt-get installsamdump2 -y
+apt-get installsipcrack -y
+apt-get installsucrack -y
+apt-get installsmbmap -y
+apt-get installrsmangler -y
+apt-get installstatsgen -y
+apt-get installchirp -y
+apt-get installcowpatty -y
+apt-get installfern-wifi-cracker -y
+apt-get installkismet -y
+apt-get installmdk3 -y
+apt-get installmfoc -y
+apt-get installmfterm -y
+apt-get installpixiewps -y
+apt-get installwifite -y
+apt-get installhackrf -y
+apt-get installbluelog -y
+apt-get installblueranger -y
+apt-get installbluesnarfer -y
+apt-get installbtscanner -y
+apt-get installredfang -y
+apt-get installspooftooph -y
+apt-get installmfcuk -y
+apt-get installasleap -y
+apt-get installcowpatty -y
+apt-get installeapmd5pass -y
+apt-get installwifi-honey -y
+apt-get installbytecode-viewer -y
+apt-get installclang -y
+apt-get installdex2jar -y
+apt-get installedb-debugger -y
+apt-get installjadx -y
+apt-get installjavasnoop -y
+apt-get installradare2 -y
+apt-get installbeef -y
+apt-get installsqlmap -y
+apt-get installtermineter -y
+apt-get installdriftnet -y
+apt-get installettercap-graphical -y
+apt-get installmacchanger -y
+apt-get installmitmproxy -y
+apt-get installnetsniff-ng -y
+apt-get installresponder -y
+apt-get installwireshark -y
+apt-get installdarkstat -y
+apt-get installdnschef -y
+apt-get installdsniff -y
+apt-get installhexinject -y
+apt-get installsslsniff -y
+apt-get installtcpflow -y
+apt-get installrebind -y
+apt-get installsniffjoke -y
+apt-get installsslsplit -y
+apt-get installtcpreplay -y
+apt-get installyersinia -y
+apt-get installbackdoor-factory -y
+apt-get installnishang -y
+apt-get installproxychains4 -y
+apt-get installweevely -y
+apt-get installcymothoa -y
+apt-get installsbd -y
+apt-get installlaudanum -y
+apt-get installiodine -y
+apt-get installmiredo -y
+apt-get installproxytunnel -y
+apt-get installpwnat -y
+apt-get installsslh -y
+apt-get installstunnel4 -y
+apt-get installudptunnel -y
+apt-get installautopsy -y
+apt-get installbinwalk -y
+apt-get installchkrootkit -y
+apt-get installforemost -y
+apt-get installgalleta -y
+apt-get installhashdeep -y
+apt-get installdc3dd -y
+apt-get installdcfldd -y
+apt-get installextundelete -y
+apt-get installmissidentify -y
+apt-get installpst-utils -y
+apt-get installreglookup -y
+apt-get installddrescue -y
+apt-get installguymager -y
+apt-get installpdfid -y
+apt-get installpdf-parser -y
+apt-get installforemost -y
+apt-get installmagicrescue -y
+apt-get installpasco -y
+apt-get installpev -y
+apt-get installrecoverjpeg -y
+apt-get installrifiuti -y
+apt-get installrifiuti2 -y
+apt-get installsafecopy -y
+apt-get installcalpel -y
+apt-get installscrounge-ntfs -y
+apt-get installcutycapt -y
+apt-get installpipal -y
+apt-get installrecordmydesktop -y
---
+++ scr_link.sh
@@ -0,0 +1,21 @@
+wget http://www.morningstarsecurity.com/downloads/bing-ip2hosts-0.4.tar.gz && tar -xzvf bing-ip2hosts-0.4.tar.gz && cp bing-ip2hosts-0.4/bing-ip2hosts /usr/local/bin/
+apt-get install git && git clone https://github.com/stasinopoulos/commix.git commix && cd commix && python ./commix.py --install
+
+echo 'download page : http://www.cqure.net/wp/tools/database/dbpwaudit/'
+apt-get install git && git clone git://git.kali.org/packages/gsd.git
+
+apt-get install git && git clone git://git.kali.org/packages/bluemaho.git
+							
+apt-get install git && git clone git://git.kali.org/packages/bluepot.git
+apt-get install git && git clone git://git.kali.org/packages/gr-scan.git
+apt-get install git && git clone https://github.com/stasinopoulos/commix.git commix && cd commix && python ./commix.py --install
+apt-get install git && git clone git://git.kali.org/packages/webslayer.git
+apt-get install git && git clone git://git.kali.org/packages/isr-evilgrade.git
+apt-get install git && git clone https://github.com/stasinopoulos/commix.git commix && cd commix && python ./commix.py --install
+apt-get install git && git clone git://git.kali.org/packages/capstone.git
+apt-get install git && git clone git://git.kali.org/packages/distorm3.git
+apt-get install git && git clone git://git.kali.org/packages/inundator.git
+apt-get install git && git clone git://git.kali.org/packages/dbpwaudit.git
+echo 'please visit : https://www.thc.org/thc-hydra/' 
+echo 'please visit : http://www.leidecker.info/projects/phrasendrescher/index.shtml' 
+git clone https://github.com/LionSec/wifresti.git && cp wifresti/wifresti.py /usr/bin/wifresti && chmod +x /usr/bin/wifresti && wifresti
